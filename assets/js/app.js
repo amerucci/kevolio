1c77489c691cd56133991087674a08a6d201fa96
amerucci <a.merucci@onlineformapro.com>
2023-05-02 16:24:40 +0200

    page transition function js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,36 @@
-let spans = document.querySelectorAll(".titleContainer span")
-spans[0].before.style="left:0"
+let start= document.querySelector("#startTour")
+let homeEnter= document.querySelector("#homeEnter")
+let home= document.querySelector("#home")
+let contact= document.querySelector("#contactLink")
 
 
+start.addEventListener("click", function(){
+    loadPage(homeEnter, start.getAttribute("pagetoload"), 500)
+
+})
+
+contact.addEventListener("click", function(){
+    loadPage(home, contact.getAttribute("pagetoload"), 500)
+
+})
+
+
+
+contact
+
+function loadPage(hide, show, timing){
+    document.querySelector("#transition").classList.add('open')
+    setTimeout(() => {
+        document.querySelector("#transition").style="bottom:100%"  
+        hide.style="display:none"
+        document.querySelector("#"+show).style="display:flex;"
+
+
+    }, timing);
+    setTimeout(() => {
+        document.querySelector("#transition").style="bottom:0%"  
+        document.querySelector("#transition").classList.remove('open')
+
+    }, timing+500);
+}
 
